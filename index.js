8944a950592cc6586473237e100a0560f90880b5
pazguille <guille87paz@gmail.com>
2012-12-20 06:04:50 -0300

    Convert indentation to Spaces

--- index.js
+++ index.js
@@ -12,60 +12,60 @@ exports = module.exports = Editable;
  * Editable
  */
 function Editable(el, title) {
-	this.init(el, title);
+    this.init(el, title);
 }
 
 Emitter(Editable.prototype);
 
 Editable.prototype.init = function (el, title) {
-	var that = this;
+    var that = this;
 
-	that.el = el;
-	that.title = title || 'Click to edit.';
-	that.content = that.el.innerHTML;
+    that.el = el;
+    that.title = title || 'Click to edit.';
+    that.content = that.el.innerHTML;
 
-	that.el.setAttribute('contentEditable', true);
-	that.el.setAttribute('title', that.title);
+    that.el.setAttribute('contentEditable', true);
+    that.el.setAttribute('title', that.title);
 
-	// W3C
-	if (that.el.addEventListener) {
-		that.el.addEventListener('keydown', function () {
-			that.onKeydown(event);
-		}, false);
-		that.el.addEventListener('blur', function () {
-			that.onBlur();
-		}, false);
+    // W3C
+    if (that.el.addEventListener) {
+        that.el.addEventListener('keydown', function () {
+            that.onKeydown(event);
+        }, false);
+        that.el.addEventListener('blur', function () {
+            that.onBlur();
+        }, false);
 
-	// IE
-	} else if (that.el.attachEvent)  {
-		that.el.attachEvent('onkeydown', function (event) {
-			that.onKeydown(event)
-		});
-		that.el.attachEvent('onblur', function () {
-			that.onBlur();
-		});
-	}
+    // IE
+    } else if (that.el.attachEvent)  {
+        that.el.attachEvent('onkeydown', function (event) {
+            that.onKeydown(event)
+        });
+        that.el.attachEvent('onblur', function () {
+            that.onBlur();
+        });
+    }
 }
 
 Editable.prototype.onKeydown = function (event) {
-	var esc = (event.which === 27),
-		enter = (event.which === 13);
+    var esc = (event.which === 27),
+        enter = (event.which === 13);
 
-	if (esc) {
-		this.el.innerHTML = this.content;
-		this.el.blur();
-		this.emit('cancel', this.content);
+    if (esc) {
+        this.el.innerHTML = this.content;
+        this.el.blur();
+        this.emit('cancel', this.content);
 
-	} else if (enter) {
-		this.el.blur();
-	}
+    } else if (enter) {
+        this.el.blur();
+    }
 }
 
 Editable.prototype.onBlur = function () {
-	if (this.content !== this.el.innerHTML) {
-		this.content = this.el.innerHTML;
-		this.emit('done', this.content);
-	}
+    if (this.content !== this.el.innerHTML) {
+        this.content = this.el.innerHTML;
+        this.emit('done', this.content);
+    }
 }
 
 
