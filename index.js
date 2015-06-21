e26bec69449477c5ac8f1566935784929ae3d970
Guille Paz <guille87paz@gmail.com>
2015-06-21 10:00:28 -0300

    Refactor Editable module.

--- index.js
+++ index.js
@@ -1,74 +1,113 @@
+'use strict';
+
 /**
  * Module dependencies.
  */
-var Emitter = require('emitter');
+var Emitter = require('jvent');
 
 /**
- * Expose Editable
+ * Normalize events
  */
-exports = module.exports = Editable;
+var on = window.addEventListener ? 'addEventListener' : 'attachEvent',
+    off = window.removeEventListener ? 'removeEventListener' : 'detachEvent',
+    prefix = on !== 'addEventListener' ? 'on' : '';
+
+/**
+ * extend
+ */
+function extend(destination, from) {
+  for (var prop in from) {
+    if (from[prop]) {
+      destination[prop] = from[prop];
+    }
+  }
+  return destination;
+}
+
+/**
+ * Inherits
+ */
+function inherits(child, uber) { child.prototype = extend(child.prototype || {}, uber.prototype); }
 
 /**
  * Editable
  */
 function Editable(el, title) {
-    if (!(this instanceof Editable)) return new Editable(el, title);
-    this.init(el, title);
+  if (!(this instanceof Editable)) { return new Editable(el, title); }
+  this.initialize(el, title);
 }
 
-Emitter(Editable.prototype);
+/**
+ * Inherits from Emitter
+ */
+inherits(Editable, Emitter);
 
-Editable.prototype.init = function (el, title) {
-    var that = this;
+/**
+ * Initialize
+ */
+Editable.prototype.initialize = function(el, title) {
+  this.el = el;
+  this.title = title || 'Click to edit.';
+  this.content = this.el.innerHTML;
+  this.initialContent = this.el.innerHTML;
 
-    that.el = el;
-    that.title = title || 'Click to edit.';
-    that.content = that.el.innerHTML;
+  this.el.setAttribute('title', this.title);
+  this.el.setAttribute('tabindex', '0');
 
+  this.bindEvents();
+};
+
+/**
+ * Bind events
+ */
+Editable.prototype.bindEvents = function() {
+  var that = this;
+  this.el[on](prefix + 'focus', function() {
     that.el.setAttribute('contentEditable', true);
-    that.el.setAttribute('title', that.title);
+  }, false);
+  this.el[on](prefix + 'keydown', this.onKeydown.bind(this), false);
+  this.el[on](prefix + 'blur', this.onBlur.bind(this), false);
+};
 
-    // W3C
-    if (that.el.addEventListener) {
-        that.el.addEventListener('keydown', function () {
-            that.onKeydown(event);
-        }, false);
-        that.el.addEventListener('blur', function () {
-            that.onBlur();
-        }, false);
+/**
+ * onKeydown
+ */
+Editable.prototype.onKeydown = function(eve) {
+  var esc = eve.which === 27,
+      enter = eve.which === 13;
 
-    // IE
-    } else if (that.el.attachEvent)  {
-        that.el.attachEvent('onkeydown', function (event) {
-            that.onKeydown(event)
-        });
-        that.el.attachEvent('onblur', function () {
-            that.onBlur();
-        });
-    }
-}
+  if (esc) {
+    this.el.innerHTML = this.content;
+    this.el.blur();
+    this.emit('cancel', this.content);
+  } else if (enter) {
+    this.el.blur();
+  }
+};
 
-Editable.prototype.onKeydown = function (event) {
-    var esc = (event.which === 27),
-        enter = (event.which === 13);
+/**
+ * onBlur
+ */
+Editable.prototype.onBlur = function() {
+  this.el.removeAttribute('contentEditable');
+  if (this.content !== this.el.innerHTML) {
+    this.content = this.el.innerHTML;
+    this.emit('done', this.content);
+  }
+};
 
-    if (esc) {
-        this.el.innerHTML = this.content;
-        this.el.blur();
-        this.emit('cancel', this.content);
+/**
+ * Destroy
+ */
+Editable.prototype.destroy = function() {
+  this.el.innerHTML = this.initialContent;
+  this.el.removeAttribute('contentEditable');
+  this.el.removeAttribute('title');
+  this.el[off](prefix + 'keydown');
+  this.el[off](prefix + 'blur');
+};
 
-    } else if (enter) {
-        this.el.blur();
-    }
-}
-
-Editable.prototype.onBlur = function () {
-    this.el.removeAttribute('contentEditable');
-    if (this.content !== this.el.innerHTML) {
-        this.content = this.el.innerHTML;
-        this.emit('done', this.content);
-    }
-}
-
-
-Editable.prototype.destroy = function () {}
+/**
+ * Expose Editable
+ */
+module.exports = Editable;
